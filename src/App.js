73576130c116a280c2c1f86e93f41d5f531880d9
Robert <robertquillaslopez@gmail.com>
2022-07-19 10:32:34 -0500

    proyecto base

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Robert from "./component/Robert";
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <Robert />
+      </section>
+      <section>
+        CARLOS
+      </section>
+      <section>
+        MIGUEL
+      </section>
+      <section>
+        NATALIA
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Robert.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Robert = () => {
+  return (
+    <div>Hola soy Robert</div>
+  )
+}
+
+export default Robert
