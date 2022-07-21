34e7b7f058019068f44c897cdbf2df9d8c333735
natvvgls <natalia.d.m975@gmail.com>
2022-07-21 12:00:11 -0500

    Añadi y comprimi los SVG

---
+++ src/component/SVG/footer/Facebook.jsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+const Facebook = (props) => (
+  <svg
+    width={53}
+    height={53}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <ellipse cx={26.481} cy={26.5} rx={26.481} ry={26.5} fill="#43311F" />
+    <path
+      d="M30.62 18.156h2.357v-3.97c-.41-.067-1.814-.186-3.43-.186-3.403 0-5.72 2.065-5.72 5.876v3.282h-3.84v4.447h3.84V39h4.595V27.605h3.827l.57-4.447h-4.397v-2.845c0-1.284.357-2.157 2.198-2.157Z"
+      fill="#fff"
+    />
+  </svg>
+)
+
+export default Facebook
